520139eb46320401251423c17e0a3bceb9b4116e
余金 <metal@192.168.0.100>
2020-03-22 15:22:54 +0800

    chg forbidden and no page

--- src/views/403.vue
+++ src/views/403.vue
@@ -1,10 +1,63 @@
 <template>
-    <div>Forbidden页面</div>
+    <div class="fobidden-container">
+        <div class="fobidden-content">
+            <Fobidden />
+        </div>
+        <div class="fobidden-title">403</div>
+        <div class="fobidden-tips">抱歉，你无权访问该页面。</div>
+        <a-button
+            type="primary"
+            class="fobidden-button"
+            @click="handleGoHome"
+        >返回首页</a-button>
+    </div>
 </template>
 
 <script>
-export default {};
+//是一个svg组件
+import Fobidden from "@/assets/403.svg";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Fobidden
+  },
+  methods: {
+    handleGoHome() {
+      this.$router.push("/");
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.fobidden-container {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  padding-top: 100px;
+}
+.fobidden-content {
+  text-align: center;
+  width: 250px;
+  height: 295px;
+  margin: auto;
+  margin-bottom: 24px;
+}
+.fobidden-title {
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 24px;
+  line-height: 1.8;
+  text-align: center;
+}
+.fobidden-tips {
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 14px;
+  line-height: 1.6;
+  text-align: center;
+}
+.fobidden-button {
+  margin-top: 32px;
+}
 </style>
--- src/views/404.vue
+++ src/views/404.vue
@@ -1,26 +1,63 @@
 <template>
-    <div style="text-align: center; padding-top: 200px">
-        <IconFont
-            type="icon-icon-404"
-            style="font-size: 100px"
-        />
-        <br />
-        <!-- <Agree /> -->
+  <div class="not-found-container">
+    <div class="not-found-content">
+      <NotFound />
     </div>
+    <div class="not-found-title">404</div>
+    <div class="not-found-tips">抱歉，你访问的页面不存在。</div>
+    <a-button
+      type="primary"
+      class="not-found-button"
+      @click="handleGoHome"
+    >返回首页</a-button>
+  </div>
 </template>
 
 <script>
 //是一个svg组件
-// import Agree from "@/assets/agree.svg";
+import NotFound from "@/assets/404.svg";
 export default {
   data() {
     return {};
   },
   components: {
-    // Agree
+    NotFound
+  },
+  methods: {
+    handleGoHome() {
+      this.$router.push("/");
+    }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.not-found-container {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  padding-top: 100px;
+}
+.not-found-content {
+  text-align: center;
+  width: 250px;
+  height: 295px;
+  margin: auto;
+  margin-bottom: 24px;
+}
+.not-found-title {
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 24px;
+  line-height: 1.8;
+  text-align: center;
+}
+.not-found-tips {
+  color: rgba(0, 0, 0, 0.45);
+  font-size: 14px;
+  line-height: 1.6;
+  text-align: center;
+}
+.not-found-button {
+  margin-top: 32px;
+}
 </style>
